3e3684d9f08d89323f05e5ab242678fd13373d6f
AshutoshDM1 <downlodemaster1@gmail.com>
2026-06-11 23:40:50 +0530

    feat: add profile fold component and open graph image asset

--- src/views/Home/Folds/Profile.tsx
+++ src/views/Home/Folds/Profile.tsx
@@ -11,17 +11,17 @@ const Profile = () => {
           <div className="size-24 border shadow-sm  rounded-sm p-1 overflow-hidden">
             <Image
               src="/avatar/avatar.png"
-              height={50}
-              width={50}
+              height={100}
+              width={100}
               className="size-full object-cover rounded-sm border "
               alt="profile Image"
             />
           </div>
           <div className="flex flex-col justify-end pb-2">
-            <h2 className="font-medium text-xl leading-5 flex items-center gap-1.5">
+            <h1 className="font-medium text-xl leading-5 flex items-center gap-1.5">
               Ashutosh Tiwari{" "}
               <BadgeCheck className="size-[20px] fill-blue-500  text-white dark:text-black shrink-0" />
-            </h2>
+            </h1>
             <p className="text-sm leading-5 ">Software Engineer</p>
           </div>
         </div>
